fc7f11ad49d517856f2b5fe07bcb1bbe44be0b04
Jeanne Castillo <jcaron27@gmail.com>
2017-09-19 12:03:54 -0400

    create tweetBank.js

--- app.js
+++ app.js
@@ -5,33 +5,31 @@ app.use(volleyball);
 const morgan = require('morgan');
 morgan('tiny');
 const nunjucks = require('nunjucks');
-
-
 const http = require('http');
 const server = http.createServer();
 const PORT = 3000;
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(output);
+// });
+
 app.use(function (req, res, next) {
-  // do your logging here
-  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
   console.log(`${req.method} ${req.url} ${res.statusCode}`);
-  if (req.url.split('/')[1] === 'special') {
-    console.log(`You reached the special area`);
-  }
+  // if (req.url.split('/')[1] === 'special') {
+  //   console.log(`You reached the special area`);
+  // }
   next();
 });
 
-
 app.get('/', function (req, res) {
-  res.send('Welcome to Twitter JS!');
-});
-
-app.get('/news', function (req, res) {
-  res.send('Fake NEWS!!');
-});
-
-app.get('/special', function (req, res) {
-  res.send('You reached the special area!!');
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(3000, function () {
@@ -39,18 +37,6 @@ app.listen(3000, function () {
 });
 
 
-var locals = {
-  title: 'An Example',
-  people: [
-      { name: 'Gandalf'},
-      { name: 'Frodo' },
-      { name: 'Hermione'}
-  ]
-};
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-  if (err) {
-    throw err;
-  }
-  console.log(output);
-});
+
+
+
